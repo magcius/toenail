06ecf45955ae9605f74a67741f382e24c48ba146
Jasper St. Pierre <jstpierre@mecheye.net>
2011-08-24 09:10:30 -0400

    semi it up.

--- ailment.py
+++ ailment.py
@@ -107,14 +107,14 @@ def java_patch():
     objRaw['getAttr'] = object.__getattribute__;
     objRaw['setAttr'] = object.__setattr__;
 
-    unary = ctypes.CFUNCTYPE(ctypes.py_object, ctypes.py_object)
+    unary = ctypes.CFUNCTYPE(ctypes.py_object, ctypes.py_object);
     binary = ctypes.CFUNCTYPE(ctypes.py_object, ctypes.py_object, ctypes.py_object);
     func = ctypes.CFUNCTYPE(ctypes.c_void_p);
 
     class PyNumberMethods(ctypes.Structure):
         _fields_ = ([("add", binary)] +
                     zip("a"*6, (func,)*6) +
-                    [("negative", unary), ("positive", unary)])
+                    [("negative", unary), ("positive", unary)]);
 
     class PyTypeObject(ctypes.Structure):
         _fields_ = ([("ob_refcnt", ctypes.c_int), ("ob_type", ctypes.c_void_p),
@@ -123,23 +123,23 @@ def java_patch():
                     zip("a"*6, (func,)*6) + [("tp_as_number", ctypes.POINTER(PyNumberMethods))]);
 
     def _install(pytype):
-        rawtypename = getattr(ctypes.pythonapi, "Py%s_Type" % pytype.__name__.capitalize())
-        typeobj = ctypes.cast(rawtypename, ctypes.POINTER(PyTypeObject))
-        _negative = pytype.__neg__
+        rawtypename = getattr(ctypes.pythonapi, "Py%s_Type" % pytype.__name__.capitalize());
+        typeobj = ctypes.cast(rawtypename, ctypes.POINTER(PyTypeObject));
+        _negative = pytype.__neg__;
 
         class IncrementUnary(pytype):
             def __pos__(self):
-                return int(self)+1
+                return int(self)+1;
 
         class DecrementUnary(pytype):
             def __neg__(self):
-                return _negative(int(self))-1
+                return _negative(int(self))-1;
 
         def decrement_unary(v):
-            return DecrementUnary(_negative(v))
+            return DecrementUnary(_negative(v));
 
-        typeobj.contents.tp_as_number.contents.positive = unary(IncrementUnary)
-        typeobj.contents.tp_as_number.contents.negative = unary(decrement_unary)
+        typeobj.contents.tp_as_number.contents.positive = unary(IncrementUnary);
+        typeobj.contents.tp_as_number.contents.negative = unary(decrement_unary);
 
     for pytype in (int, long):
         _install(pytype)
